0b7c354807b3ff80e870441ea969772964a86d04
Chris May <chrismay50@gmail.com>
2011-06-06 22:30:20 +0100

    ex 3

--- src/test/scala/Ex3.scala
+++ src/test/scala/Ex3.scala
@@ -2,18 +2,23 @@ import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 
 class Ex3Suite extends FunSuite with ShouldMatchers {
-  lazy val naturals: Stream[Int] = Stream.cons(1, naturals.map(_ + 1))
+  lazy val factors: Stream[Long] = Stream.cons(2, factors.map(_ + 1))
 
- def pickFactor(i:Int):Int={
-      println("getting factor for " + i);
-       naturals.takeWhile(_<i/2).foldLeft(i)((num,maybeFactor)=>(if (num % maybeFactor ==0)  num/maybeFactor else num))
+  def lowestFactor(i: Long): Option[Long] = {
+    factors.takeWhile(_ < i / 2).find((j: Long) => (i % j == 0))
   }
-  def ex3(i:Int):Int={
-    var start = pickFactor(i)
-    var next = pickFactor(i/start);
-    if (start == next  ) start  else ex3(next)
+  def ex3(i: Long): Long = {
+   val f = lowestFactor(i)
+   f match {
+      case None => i
+      case _ => ex3(i/f.get)		
+    }
   }
-  test("naturals from 1 to 5 look right")(naturals.take(5).mkString(",") should equal("1,2,3,4,5"))
-  
+  test("factors from 1 to 5 look right")(factors.take(5).mkString(",") should equal("2,3,4,5,6"))
+
   test("largest prime factor of 12 is 3")(ex3(12) should equal(3))
-}
+  test("largest prime factor of 25 is 5")(ex3(25) should equal(5))
+  test("largest prime factor of 600851475143 is 6857")(ex3(600851475143L) should equal(6857))
+  test("largest prime factor of 87625999 is 1471")(ex3(87625999) should equal(1471))
+  test("largest prime factor of 59569 is 839")(ex3(59569) should equal(839))
+} 
