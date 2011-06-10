0963aadbe685ecf10760b025e4a25eaf9e38fb4e
Chris May <chrismay50@gmail.com>
2011-06-10 19:38:34 +0100

    made ex3 use less heap

--- src/test/scala/Ex3.scala
+++ src/test/scala/Ex3.scala
@@ -1,22 +1,21 @@
 import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
+import Iterator.iterate
 
-class Ex3Suite extends FunSuite with ShouldMatchers {
-  
-  
-  lazy val naturals: Stream[Long] = Stream.cons(1, naturals.map(_ + 1))
-
+@RunWith(classOf[JUnitRunner])
+class Ex3Suite extends FunSuite  with ShouldMatchers {
+ 
   def ex3(compound: Long): Long = {
-   naturals.drop(1).takeWhile(_ < compound/ 2).find(
-        maybeFactor => (compound % maybeFactor == 0)).map(
+   iterate(2L)(_+1).takeWhile(_ < compound/ 2).find(
+        maybeFactor => (compound % maybeFactor == 0) ).map(
             factor=>ex3(compound/factor)).getOrElse(compound)
   }
   
-  test("factors from 1 to 5 look right")(naturals.take(5).mkString(",") should equal("1,2,3,4,5"))
-
   test("largest prime factor of 12 is 3")(ex3(12) should equal(3))
   test("largest prime factor of 25 is 5")(ex3(25) should equal(5))
   test("largest prime factor of 600851475143 is 6857")(ex3(600851475143L) should equal(6857))
   test("largest prime factor of 87625999 is 1471")(ex3(87625999) should equal(1471))
   test("largest prime factor of 59569 is 839")(ex3(59569) should equal(839))
-} 
+}
